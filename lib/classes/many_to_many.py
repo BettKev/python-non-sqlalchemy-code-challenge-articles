6a9213259b3713f88708bb8a26ef69625c707814
Kevin Bett <prodigy4614@gmail.com>
2024-12-06 11:13:32 +0300

    updated code

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -4,9 +4,10 @@ class Article:
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self._title = title  # Use a private variable to store the title
+        self._title = title  # Use a private variable
         Article.all.append(self)
     
+    #getter
     @property
     def title(self):
         return self._title  # Provide read-only access to the title
@@ -15,9 +16,18 @@ class Author:
     def __init__(self, name):
         self._name = name
 
+    #getter
     @property
     def name(self):
-        return self._name
+        return self._name # Provide read-only access to the name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+        else:
+            raise ValueError("Name must be a non-empty string.")
+
 
     def articles(self):
         pass
